fa1ca3f7d7ba89c426321c016874da3fe036c119
AlmirPaulo <ap.freelas@gmail.com>
2021-02-24 20:55:51 -0300

    update as deploy

--- main.py
+++ main.py
@@ -8,45 +8,55 @@
 # Email: ap.freelas@gmail.com
 
 
-from telegram.ext import *
-from telegram import *
-import side
-# deploy version
-# from keep_alive import ?
-# import os 
+from telegram.ext import (CallbackContext,
+ Updater, 
+ Filters, 
+ CommandHandler,
+  MessageHandler, 
+  CallbackQueryHandler 
+  )
+from telegram import (Update, InlineKeyboardButton, InlineKeyboardMarkup, ParseMode)
+import side, os 
 
 #Variables
-token = open('note_token', 'r').read()
+token = os.getenv('TOKEN')
 
 def start(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('''
+  update.message.reply_text('''
     Welcome! \n Type the name of your favorite artist or song to get your tabs. ''')
 
 def help(update: Update, context: CallbackContext) -> None:
-    update.message.reply_text('''Just type the name of your favorite artist or song. If something goes wrong, try in another way (just the first name of the artist, for instance). \n I will give you all the available tab options, you click, get the link and enjoy.''')
+  update.message.reply_text('''Just type the name of your favorite artist or song. If something goes wrong, try in another way (just the first name of the artist, for instance). \n I will give you all the available tab options, you click, get the link and enjoy.''')
 
 def get_tabs(update: Update, context: CallbackContext) -> None:
-    user_input = update.message.text
-    keyboard = []
-    for i in side.api_req(user_input):
-         keyboard.append([InlineKeyboardButton(f"{i['artist']['name']} - {i['title']}", callback_data=i['id'])])
+  user_input = update.message.text
+  keyboard = []
+  for i in side.api_req(user_input):
+    keyboard.append([InlineKeyboardButton(f"{i['artist']['name']} - {i['title']}", callback_data=i['id'])])
 
-    reply = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text('Choose your tab: ', reply_markup=reply)
+  reply = InlineKeyboardMarkup(keyboard)
+  update.message.reply_text('Choose your tab: ', reply_markup=reply)
 
 def button(update: Update, context: CallbackContext) -> None:
-    query = update.callback_query
-    query.answer()
-    query.edit_message_text(text=f"Here is your tabs: http://www.songsterr.com/a/wa/song?id={query.data}\n Enjoy!")
+  query = update.callback_query
+  query.answer()
+  query.edit_message_text(text=f"<a href= 'http://www.songsterr.com/a/wa/song?id={query.data}'>Click here</a> to get your tabs.\n Enjoy!", parse_mode=ParseMode.HTML)
 
 def main():
-    updater = Updater(token.strip())
 
-    updater.dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, get_tabs))
-    updater.dispatcher.add_handler(CallbackQueryHandler(button))
-    updater.dispatcher.add_handler(CommandHandler('start', start))
-    updater.dispatcher.add_handler(CommandHandler('help', help))
-    updater.start_polling()
-    updater.idle()
+    #Token
+  updater = Updater(token.strip())
+
+    #Commands
+  updater.dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, get_tabs))
+  updater.dispatcher.add_handler(CallbackQueryHandler(button))
+  updater.dispatcher.add_handler(CommandHandler('start', start))
+  updater.dispatcher.add_handler(CommandHandler('help', help))
+    
+   #Server
+  side.keep_alive()
+    #Main Loop
+  updater.start_polling()
+  updater.idle()
 
 main()
--- side.py
+++ side.py
@@ -1,13 +1,24 @@
 import requests as req
+from flask import Flask
+from threading import Thread
 
-# Acessar musica pelo nome
+
+#API request
 def api_req(pattern):
     url = "http://www.songsterr.com/a/ra/songs.json?pattern="
     resp = req.get(url+pattern.lower())
     return resp.json()
-        #return f"{i['artist']['name']} - {i['title']} - {i['id']}"
 
-#print(api_req('You know Im no good'))
+#Server
+app = Flask('')
 
+@app.route('/')
+def home():
+  return"It's Alive!"
+  
+def run():
+  app.run(host ='0.0.0.0', port = 8080)
 
-
+def keep_alive():
+  t = Thread(target = run)
+  t.start()
